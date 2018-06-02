a169efbc7b9159eee6aacf003768fecfdea6641b
Laura Gabriela Hernandez <gaby.bernabe@tum.de>
2018-06-02 21:22:45 +0200

    added navigation

---
+++ src/components/Navigation.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Navigation extends React.PureComponent{
+    render(){
+        return(
+            <ul className='pure-menu-list'>
+                <li className='pure-menu-item'></li>
+                    <a href='#' className='pure-menu-link'
+
+            </ul>
+        );
+    }
+}
+
+export default Navigation;
